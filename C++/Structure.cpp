4ebcd03c493e5d13b77ba5117bb5d510c5d9247e
urmillive <urmillive@gmail.com>
2024-10-03 08:52:45 +0530

    Refactor C++ code to use string instead of char array for student name and department

--- C++/Structure.cpp
+++ C++/Structure.cpp
@@ -1,4 +1,7 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <string> // Include the string library
+using namespace std;
 
 struct Rectangle
 {
@@ -6,11 +9,46 @@ struct Rectangle
     int breadth;
 };
 
+struct Student
+{
+    int roll;
+    string name; // Use string instead of char array
+    string dept; // Use string instead of char array
+};
+
+struct Card
+{
+    int face;
+    int shape;
+    int color;
+};
+
 int main()
 {
-    struct Rectangle r;
+    Rectangle r;
     r.length = 10;
     r.breadth = 5;
-    printf("Area of rectangle: %d", r.length * r.breadth);
+    cout << "Area of rectangle: " << r.length * r.breadth << endl;
+
+    Student s;
+    s.roll = 1;
+    s.name = "Ram"; // Direct assignment is now possible
+    s.dept = "CSE"; // Direct assignment is now possible
+    cout << "Roll: " << s.roll << ", Name: " << s.name << ", Dept: " << s.dept << endl;
+
+    struct Card deck[52] =
+        {
+            {1, 0, 0},
+            {2, 0, 0},
+            {1, 1, 0},
+            // Add more elements here
+        };
+
+    for (int i = 0; i < 52; i++)
+    {
+        printf("Card %d: %d %d %d\n", i, deck[i].face, deck[i].shape, deck[i].color);
+    }
+
+    printf("Card: %d %d %d\n", deck[0].face, deck[0].shape, deck[0].color);
     return 0;
 }
